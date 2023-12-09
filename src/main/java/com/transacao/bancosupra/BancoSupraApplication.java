9dd04d7dcd169f89ee34a792d535a9dd6f5b124a
Wesley.Lima <wesleyaz0909@gmail.com>
2023-12-09 15:39:36 -0300

    initial commit

---
+++ src/main/java/com/transacao/bancosupra/BancoSupraApplication.java
@@ -0,0 +1,13 @@
+package com.transacao.bancosupra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSupraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoSupraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transacao/bancosupra/BancoSupraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transacao.bancosupra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSupraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
